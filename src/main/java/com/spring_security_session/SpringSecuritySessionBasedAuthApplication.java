b902ccb040c3208bcd8f02f6af6f76c5923ddcf0
Agbame-Julius <juliusagbame13@gmail.com>
2025-09-29 17:53:34 +0100

    chore: add project setup

---
+++ src/main/java/com/spring_security_session/SpringSecuritySessionBasedAuthApplication.java
@@ -0,0 +1,13 @@
+package com.spring_security_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySessionBasedAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySessionBasedAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_security_session/SpringSecuritySessionBasedAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security_session;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySessionBasedAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
